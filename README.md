d682e211a83b9eb6bdac8501a0ea5246c807cf69
AnnaHeorhiieva <anna.heorhiieva@gmail.com>
2022-06-12 15:37:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-5fvt1b
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-5fvt1b)
---
+++ child/Child.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Child(): JSX.Element {
+  return <div>Child component fjf</div>;
+}
+
+export default Child;
